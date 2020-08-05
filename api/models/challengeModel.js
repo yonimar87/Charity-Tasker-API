71159ad5ca054779b975202a396394279fb51f41
yonathan marlow <yonathanmarlow@hotmail.com>
2020-08-05 16:55:51 +1000

    adding likes

--- api/models/challengeModel.js
+++ api/models/challengeModel.js
@@ -1,51 +1,48 @@
-const mongoose = require('mongoose');
-const { text } = require('body-parser');
+const mongoose = require("mongoose");
+// const { text } = require("body-parser");
 
 const { Schema } = mongoose;
 
 const ChallengeSchema = new Schema(
-    {
-        category: {
-            type: String,
-            required: 'Must have a category'
-        },
-
-        description: {
-            type: String
-        },
-
-        name: {
-            type: String,
-            required: "Must have a challenge name"
-        },
-
-        shortDescription: {
-            type: String,
-            required: 'Must have an at least a short Description'
-
-        },
-
-        goal: {
-            type: Number,
-            required: 'Needs a goal'
-        },
-
-        video_url: {
-            type: String
-        },
-
-        creator_id: {
-            type: String,
-            required: "HAS TO HAPPEN"
-        },
-
-        fulfilledBy_id: {
-            type: String
-        }
-
+  {
+    category: {
+      type: String,
+      required: "Must have a category",
     },
-     { collection: 'Challenges' }
 
-)
+    description: {
+      type: String,
+    },
 
-module.exports = mongoose.model('Challenges', ChallengeSchema);
+    name: {
+      type: String,
+      required: "Must have a challenge name",
+    },
+
+    shortDescription: {
+      type: String,
+      required: "Must have an at least a short Description",
+    },
+
+    goal: {
+      type: Number,
+      required: "Needs a goal",
+    },
+
+    likes: {
+      type: Number,
+    },
+
+    creator_id: {
+      type: String,
+      required: "HAS TO HAPPEN",
+    },
+
+    fulfilledBy_id: {
+      type: String,
+    },
+  },
+  { collection: "Challenges" }
+);
+
+module.exports = mongoose.model("Challenges", ChallengeSchema);
--- server.js
+++ server.js
@@ -1,28 +1,26 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-global.Challenge = require('./api/models/challengeModel');
-global.Charity = require('./api/models/charityModel');
-global.User = require('./api/models/userModel');
-global.Donation = require('./api/models/donationModel');
-const { mongoURI } = require('./config/keys');
-const passport = require('passport');
-const users = require('./api/routes/api/users');
-const challenges = require('./api/routes/api/challenges')
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+global.Challenge = require("./api/models/challengeModel");
+global.Charity = require("./api/models/charityModel");
+global.User = require("./api/models/userModel");
+global.Donation = require("./api/models/donationModel");
+const { mongoURI } = require("./config/keys");
+const passport = require("passport");
+const users = require("./api/routes/api/users");
+const challenges = require("./api/routes/api/challenges");
 
 mongoose.Promise = global.Promise;
-mongoose.set('useFindAndModify', false);
-mongoose.connect(
-    mongoURI,
-  { useNewUrlParser: true }
-);
+mongoose.set("useFindAndModify", false);
+mongoose.connect(mongoURI, { useNewUrlParser: true });
 const PORT = process.env.PORT || 1337; // Get ready for deployment.
 const app = express();
-
+const { cloudinary } = require("./utils/cloudinary");
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true })); // Makes POST requests work
 app.use(bodyParser.json());
+app.use(express.json({ limit: "50mb" }));
 
 app.listen(PORT);
 
@@ -33,6 +31,19 @@ require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
 app.use("/api/challenges", challenges);
+app.post("/api/upload", async (req, res) => {
+  try {
+    const fileStr = req.body.data;
+    const uploadedResponse = await cloudinary.uploader.upload(fileStr, {
+      upload_preset: "samples",
+    });
+    console.log(uploadedResponse);
+    res.json({ msg: "yayyayaya" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ err: "somehting is wrong" });
+  }
+});
 // app.use("/api/charities", charities);
 // app.use("/api/donations", donations);
 
@@ -41,4 +52,4 @@ app.use((req, res) => {
   res.status(404).send({ url: `${req.originalUrl} not found` });
 });
 
-console.log(`Serving on http://localhost:${ PORT }`);
+console.log(`Serving on http://localhost:${PORT}`);
